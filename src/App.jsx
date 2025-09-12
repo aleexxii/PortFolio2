d4bc37e79b86d0a67c9039e777cb53ae7c9c722b
aleexxii <amaleexxii@gmail.com>
2025-09-12 13:13:31 +0530

    new section techstack

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import About from './sections/About'
+import TechStack from './sections/TechStack'
 
 function App() {
   return (
     <div>
       <Hero />
       <About />
+      <TechStack />
     </div>
   )
 }
---
+++ src/sections/TechStack.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TitleHeader from '../components/TitleHeader'
+
+function TechStack() {
+  return (
+    <div className='w-full h-full'>
+        <div>
+            <div className="container mx-auto md:p-0 px-5">
+                <TitleHeader title={"TECH STACK"} number={'02'} text={'My Go-To Tools for Crafting Solutions'} />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TechStack
