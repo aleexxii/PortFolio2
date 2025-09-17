649ec59fa407a17a1ce680644541e3eab01825c2
aleexxii <amaleexxii@gmail.com>
2025-09-17 22:32:52 +0530

    edit env file

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,14 +1,73 @@
-import React from "react";
-
+import { useForm } from "react-hook-form";
+import * as Z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import emailjs from "@emailjs/browser"
 function ContactForm() {
+  const intialValue = {
+    name : '',
+    email : '',
+    subject : '',
+    message : ''
+  }
+
+  const contactFormSchema = Z.object({
+    name: Z.string().nonempty("Name is required"),
+    email: Z.string().email('Invalid email').nonempty('email is required'),
+    subject: Z.string().nonempty("Subject is required"),
+    message: Z.string().nonempty("Message is required"),
+  })
+
+  const {register, handleSubmit, formState:{errors}} = useForm({
+    defaultValues: intialValue,
+    resolver: zodResolver(contactFormSchema)
+  })
+
+
+
+  const onSubmit = async(data)=>{
+    try {
+      const payload = {
+        title : data.subject,
+        name : data.name,
+        email : data.email,
+        message : data.message,
+        time : new Date().toLocaleDateString()
+      }
+
+      const serviceID = import.meta.env.VITE_SERVICE_ID;
+      const templateID = import.meta.env.VITE_TEMPLATE_ID
+      const userID = import.meta.env.VITE_PUBLIC_KEY
+      
+      await emailjs.send(serviceID, templateID, payload,{
+        publicKey: userID
+      })
+
+    } catch (error) {
+      console.log('Failed...', error);
+      alert('Failed to send message, please try again')
+    }finally{
+      alert('message send successfuly')
+    }
+  }
+
+  useEffect(()=>{
+console.log(errors);
+  },[errors])
+
+
+
   return (
-    <div className="flex-center">
+    <div onSubmit={handleSubmit(onSubmit)} className="flex-center">
       <form className="w-full text-[#a7a7a7] flex flex-col gap-7">
         <div>
           <label className="label" htmlFor="name">
             Name
           </label>
-          <input type="text" id="name" placeholder="Tommy" className="input" />
+          <input {...register('name')} type="text" id="name" placeholder="Tommy" className="input" />
+          {errors.name && (
+            <span className='text-red-500'>{errors.name.message}</span>
+          )}
         </div>
 
         <div>
@@ -16,11 +75,15 @@ function ContactForm() {
             Email
           </label>
           <input
+          {...register('email')}
             type="text"
             id="email"
             placeholder="example@gmail.com"
             className="input"
           />
+          {errors.email && (
+            <span className='text-red-500'>{errors.email.message}</span>
+          )}
         </div>
 
         <div>
@@ -28,23 +91,31 @@ function ContactForm() {
             Subject
           </label>
           <input
+          {...register('subject')}
             type="text"
             id="subject"
             placeholder="Tommy"
             className="input"
           />
+          {errors.subject && (
+            <span className='text-red-500'>{errors.subject.message}</span>
+          )}
         </div>
         <div>
           <label className="label" htmlFor="message">
             Message
           </label>
           <textarea
+          {...register('message')}
             type="text"
             id="message"
             placeholder="example@gmail.com"
             rows={"5"}
             className="input"
           />
+          {errors.message && (
+            <span className='text-red-500'>{errors.message.message}</span>
+          )}
         </div>
         <button
           type="submit"
