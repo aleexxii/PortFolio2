2b4d8a1b14ba0b0896b6638d3e0143b878d401d3
aleexxii <amaleexxii@gmail.com>
2025-09-18 13:13:23 +0530

    removed testimonils from constant index file

--- src/constants/index.js
+++ src/constants/index.js
@@ -124,37 +124,6 @@ const slides = [
   },
 ];
 
-const testimonials = [
-  {
-    name: "John Miller",
-    pos: "Founder of ModernEdge Solutions",
-    review:
-      "David Jhon turned our vision into a stunning, functional platform that our customers love. Their creativity and technical expertise truly set them apart.",
-    imgPath: "/images/client1.png",
-  },
-  {
-    name: "Emily Carter",
-    pos: "UX Designer at PixelWorks Studio",
-    review:
-      "David Jhon consistently brings fresh ideas and innovative solutions. Their passion for creativity and attention to detail elevate every project.",
-    imgPath: "/images/client2.png",
-  },
-  {
-    name: "Sarah Lopez",
-    pos: "Entrepreneur and Small Business Owner",
-    review:
-      "Exceeded my expectations with a unique and beautifully designed product that works flawlessly. Their creative touch is outstanding.",
-    imgPath: "/images/client3.png",
-  },
-  {
-    name: "David Chen",
-    pos: "Project Manager at CreativeSphere Agency",
-    review:
-      "Blends technical skills with bold creativity to deliver exceptional results. They push boundaries and elevate every project they work on.",
-    imgPath: "/images/client4.png",
-  },
-];
-
 const footerIconsList = [
   {
     name: "Facebook",
