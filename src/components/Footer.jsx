67749c2e415320aec285a5b3b5d5525836c0326b
aleexxii <amaleexxii@gmail.com>
2025-09-18 21:35:22 +0530

    completed footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { footerIconsList } from '../constants'
+import React from "react";
+import { footerIconsList } from "../constants";
 
 function Footer() {
   return (
-    <div className='w-full flex-center flex-col md:gap-10 gap-7 bg-black-300 py-10'>
-        <div className="flex items-center md:gap-16 gap-8">
-            {
-                footerIconsList.map((icon, index) => (
-                    <div key={index} className="cursor-pointer hover:translate-y-5 transition-all duration-700">
-                        <img src={icon.icon} alt={icon.name} className='md:size-11 size-8' />
-                    </div>
-                ))
-            }
-        </div>
-        <p className='font-regular md:text-lg text-sm'>
-            2025 &#169; All rights reserved
-        </p>
+    <div className="w-full flex-center flex-col md:gap-10 gap-7 bg-black-300 py-10">
+      <div className="flex items-center md:gap-16 gap-8">
+        {footerIconsList.map((icon, index) => (
+          <div
+            key={index}
+            className="cursor-pointer hover:translate-y-5 transition-all duration-700"
+          >
+            <img
+              src={icon.icon}
+              alt={icon.name}
+              className="md:size-11 size-8"
+            />
+          </div>
+        ))}
+      </div>
+      <p className="font-regular md:text-lg text-sm">
+        2025 &#169; All rights reserved
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
